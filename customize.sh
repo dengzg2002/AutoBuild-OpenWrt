6e4602f789518c7a90ea2724fc53505c980729b7
dengzg2002 <48612912+dengzg2002@users.noreply.github.com>
2023-09-29 00:45:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,10 +5,10 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.3/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
+2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
